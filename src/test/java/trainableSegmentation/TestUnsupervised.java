07db0e3d51c1a84160f4e4db9fee8ed2740b7a23
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2017-08-04 15:54:05 +0200

    Remove unused imports.

--- src/test/java/trainableSegmentation/TestUnsupervised.java
+++ src/test/java/trainableSegmentation/TestUnsupervised.java
@@ -1,23 +1,10 @@
 package trainableSegmentation;
 import ij.IJ;
 import ij.ImagePlus;
-import ij.ImageStack;
-import ij.gui.NewImage;
-import ij.process.ImageProcessor;
 import trainableSegmentation.unsupervised.ColorClustering;
-import trainableSegmentation.unsupervised.PixelClustering;
 import weka.clusterers.AbstractClusterer;
 import weka.clusterers.Canopy;
-import weka.clusterers.Clusterer;
-import weka.clusterers.SimpleKMeans;
-import weka.core.Instances;
 
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class TestUnsupervised {
