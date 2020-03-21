8234b9d72bd11476c074f6f5ab7c062d83d84903
haesleinhuepf <rhaase@mpi-cbg.de>
2020-03-21 11:27:14 +0100

    simplified demo code

--- src/main/java/net/haesleinhuepf/clijx/weka/WekaDemo.java
+++ src/main/java/net/haesleinhuepf/clijx/weka/WekaDemo.java
@@ -1,5 +1,6 @@
 package net.haesleinhuepf.clijx.weka;
 
+import hr.irb.fastRandomForest_clij.FastRandomForest;
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -10,68 +11,70 @@ import weka.classifiers.AbstractClassifier;
 
 public class WekaDemo {
     public static void main(String[] args) {
-        ImagePlus inputImp = IJ.openImage("src/test/resources/blobs.tif");
+        // Load Example data
+        ImagePlus inputImp = IJ.openImage("src/test/resources/NPC_T01_c2.tif");
         IJ.run(inputImp, "32-bit", "");
-        ImagePlus partialGroundTruthImp = NewImage.createFloatImage("label map", inputImp.getWidth(), inputImp.getHeight(), 1, NewImage.FILL_BLACK);
+        ImagePlus partialGroundTruthImp = IJ.openImage("src/test/resources/NPC_T01_c2_ground_truth.tif");
 
-        // true pixels
-        partialGroundTruthImp.setRoi(21,51,17,13);
-        partialGroundTruthImp.getProcessor().setColor(2);
-        partialGroundTruthImp.getProcessor().fill();
-
-        // false pixels
-        partialGroundTruthImp.setRoi(101,37,20,16);
-        partialGroundTruthImp.getProcessor().setColor(1);
-        partialGroundTruthImp.getProcessor().fill();
-
-        int numberOfFeatures = 11;
-
+        // init GPU
         CLIJx clijx = CLIJx.getInstance();
+
+        // push to GPU
         ClearCLBuffer input = clijx.push(inputImp);
         ClearCLBuffer partialGroundTruth = clijx.push(partialGroundTruthImp);
-        ClearCLBuffer temp = clijx.create(input);
-        ClearCLBuffer temp2 = clijx.create(input);
-        ClearCLBuffer featureStack = clijx.create(new long[] {input.getWidth(), input.getHeight(), numberOfFeatures});
-
-        int featureCount = 0;
-
-        clijx.copySlice(input, featureStack, featureCount);
-        featureCount ++;
-
-
-        for (int i = 0; i < 5; i++) {
-            double sigma = (i + 1);
-            clijx.blur(input, temp, sigma, sigma);
-
-            clijx.sobel(temp, temp2);
-
-            clijx.copySlice(temp, featureStack, featureCount);
-            featureCount ++;
-            clijx.copySlice(temp2, featureStack, featureCount);
-            featureCount ++;
-
-        }
+        ClearCLBuffer featureStack = GenerateFeatureStack.generateFeatureStack(clijx, input, "original gaussianblur=1 gaussianblur=5 sobelofgaussian=1 sobelofgaussian=5");
 
         new ImageJ();
 
+        // show input data
         clijx.show(input, "input");
-
         clijx.show(partialGroundTruth, "partial ground truth");
-
         clijx.show(featureStack, "feature stack");
 
+        // Train (internally with Fijis Trainable Segmentation)
+        CLIJxWeka cw = new CLIJxWeka(clijx, featureStack, partialGroundTruth);
 
-        net.haesleinhuepf.clijx.weka.CLIJxWeka cw = new net.haesleinhuepf.clijx.weka.CLIJxWeka(clijx, featureStack, partialGroundTruth);
-
-        AbstractClassifier classifier = cw.getClassifier();
+        FastRandomForest classifier = cw.getClassifier();
         int numberOfClasses = cw.getNumberOfClasses();
 
-        net.haesleinhuepf.clijx.weka.CLIJxWeka cw2 = new CLIJxWeka(clijx, featureStack, classifier, numberOfClasses);
+        // Predict (internally with Fijis Trainable Segmentation)
+        CLIJxWeka cw2 = new CLIJxWeka(clijx, featureStack, classifier, numberOfClasses);
 
         ClearCLBuffer buffer = cw2.getClassification();
         clijx.show(buffer, "classification");
 
+        // Predict (internally with OpenCL)
+        String openCLkernelcode = cw2.getOCL();
+        ApplyOCLWekaModel.applyOCL(clijx, featureStack, buffer, openCLkernelcode);
+        clijx.show(buffer, "classification_opencl");
+
+
+
+
+
+
+
+
+
+
+
 
         clijx.clear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
